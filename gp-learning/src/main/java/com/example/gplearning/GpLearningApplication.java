5c649fddcf4bf50c130fc51f4c6e67a85c50e6b6
jorgezhong <zhongyuxian@Oeasy.com>
2019-08-09 14:14:43 +0800

    -- gp 案例学习项目

---
+++ gp-learning/src/main/java/com/example/gplearning/GpLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.gplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpLearningApplication.class, args);
+    }
+
+}
